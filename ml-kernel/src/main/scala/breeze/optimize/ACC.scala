c64e2a31f4a091afa9c2184e4095bb50cbfd53a4
Danliran <zhouxion_422@126.com>
2020-10-26 21:25:15 +0800

    Add file license

--- ml-kernel/src/main/scala/breeze/optimize/ACC.scala
+++ ml-kernel/src/main/scala/breeze/optimize/ACC.scala
@@ -1,3 +1,9 @@
+/*
+ * This file to You under the Apache License, Version 2.0;
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ */
 package breeze.optimize
 
 import breeze.math.MutableInnerProductModule
--- ml-kernel/src/main/scala/org/apache/spark/ml/tree/impl/GradientBoostedTreesUtil.scala
+++ ml-kernel/src/main/scala/org/apache/spark/ml/tree/impl/GradientBoostedTreesUtil.scala
@@ -1,5 +1,11 @@
-package org.apache.spark.ml.tree.impl
+/*
+ * This file to You under the Apache License, Version 2.0;
+ * you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ */
 
+package org.apache.spark.ml.tree.impl
 import it.unimi.dsi.fastutil.doubles.DoubleArrayList
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.ints.IntArrayList
