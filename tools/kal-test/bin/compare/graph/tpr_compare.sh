1b83db8a1f52ec188a01bf929884c728b13f0f7a
wyy566 <531938832@qq.com>
2022-08-08 17:26:15 +0800

    fix the configuration

--- tools/kal-test/bin/compare/graph/tpr_compare.sh
+++ tools/kal-test/bin/compare/graph/tpr_compare.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
+opt_path=/tmp/graph/result/tpr/twitter_tpr/no
+raw_path=/tmp/graph/result/tpr/twitter_tpr/yes
+opt_split="\t"
+raw_split="\t"
+
 case "$1" in
 -h | --help | ?)
   echo "Usage: <path0> <path1> <split0> <split1>"
-  echo "1st argument: path of tpr result: default value [/tmp/graph/result/tpr/twitter_tpr/no]"
-  echo "2nd argument: path of tpr result: default value [/tmp/graph/result/tpr/twitter_tpr/yes]"
-  echo "3rd argument: split of the result in path0: default value [\t]"
-  echo "4th argument: split of the result in path1: default value [\t]"
+  echo "1st argument: path of tpr result: default value [${opt_path}]"
+  echo "2nd argument: path of tpr result: default value [${raw_path}]"
+  echo "3rd argument: split of the result in path0: default value [${opt_split}]"
+  echo "4th argument: split of the result in path1: default value [${raw_split}]"
   exit 0
   ;;
 esac
@@ -17,10 +22,10 @@ path1=$2
 split0=$3
 split1=$4
 
-path0=${path0:-/tmp/graph/result/tpr/twitter_tpr/no}
-path1=${path1:-/tmp/graph/result/tpr/twitter_tpr/yes}
-split0=${split0:-"\t"}
-split1=${split1:-"\t"}
+path0=${path0:-${opt_path}}
+path1=${path1:-${raw_path}}
+split0=${split0:-${opt_split}}
+split1=${split1:-${raw_split}}
 
 source conf/graph/graph_datasets.properties
 scala_version=scalaVersion
