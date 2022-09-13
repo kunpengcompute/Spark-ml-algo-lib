2c3ffed9f4b48f9f0a57a30c5b93fea57ab04fce
xiongyutian <xiongyutian@huawei.com>
2022-09-13 14:47:30 +0800

    Update RF,GBDT's result comparison solution.

--- tools/kal-test/bin/compare/ml/evaluation_compare.sh
+++ tools/kal-test/bin/compare/ml/evaluation_compare.sh
@@ -3,22 +3,25 @@ set -e
 
 case "$1" in
 -h | --help | ?)
-  echo "Usage: <path0> <path1>"
-  echo "1st argument: path of 1st result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1]"
-  echo "2nd argument: path of 2nd result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1_raw]"
+  echo "Usage: <algorithm type> <path0> <path1>"
+  echo "1st argument: type of algorithm: [classification/regression]"
+  echo "2nd argument: path of opt result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1]"
+  echo "3rd argument: path of raw result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1_raw]"
   exit 0
   ;;
 esac
 
-if [ $# -ne 2 ]; then
-  echo "Usage: <path0> <path1>"
-  echo "1st argument: path of 1st result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1]"
-  echo "2nd argument: path of 2nd result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1_raw]"
+if [ $# -ne 3 ]; then
+  echo "Usage: <algorithm type> <path0> <path1>"
+  echo "1st argument: type of algorithm: [classification/regression]"
+  echo "2nd argument: path of opt result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1]"
+  echo "3rd argument: path of raw result: eg [hdfs:///tmp/ml/result/RF/classification_epsilon_dataframe_fit1_raw]"
   exit 0
 fi
 
-path0=$1
-path1=$2
+algorithm_type=$1
+path0=$2
+path1=$3
 
 source conf/ml/ml_datasets.properties
 scala_version=scalaVersion
@@ -31,4 +34,4 @@ spark-submit \
 --executor-memory 35g \
 --executor-cores 8 \
 --driver-memory 50g \
-./lib/kal-test_${scala_version_val}-0.1.jar ${path0} ${path1}
+./lib/kal-test_${scala_version_val}-0.1.jar ${algorithm_type} ${path0} ${path1}
--- tools/kal-test/src/main/scala/com/bigdata/ml/GBDTRunner.scala
+++ tools/kal-test/src/main/scala/com/bigdata/ml/GBDTRunner.scala
@@ -131,8 +131,8 @@ object GBDTRunner {
 
       Utils.checkDirs("report")
       if(ifCheck.equals("yes")){
-        params.setIsCorrect(EvaluationVerify.compareRes(params.saveDataPath, params.verifiedDataPath, spark))
-        val writerIsCorrect = new FileWriter(s"report/!ml_isCorrect.txt", true)
+        params.setIsCorrect(EvaluationVerify.compareRes(params.saveDataPath, params.verifiedDataPath, params.algorithmType, spark))
+        val writerIsCorrect = new FileWriter(s"report/ml_isCorrect.txt", true)
         writerIsCorrect.write(s"${params.testcaseType} ${params.isCorrect} \n")
         writerIsCorrect.close()
       }
